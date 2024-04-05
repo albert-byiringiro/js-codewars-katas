c69e8855aa20c1df2915cd5d6efa19eca06fc52f
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-04-05 21:30:31 +0200

    Add Kata - Correct Errors in recognition software 2

--- day2-correctErrors.js
+++ day2-correctErrors.js
@@ -13,20 +13,20 @@ The test cases contain numbers only by mistake.
 */
 
 function correctErrors(str) {
-    let arr = [];
-    str.split("").map((char) => {
-        if (char === "5") {
-            arr.push("S");
-        } else if (char === "0") {
-            arr.push("O");
-        } else if (char === "1") {
-            arr.push("I");
-        } else {
-            arr.push(char);
-        }
-    });
+    function correctErrors(str) {
+        return str.replace(/5/g, "S").replace(/0/g, "O").replace(/1/, "I");
+    }
 
-    return arr.join("")
+    console.log(correctErrors("M00kn15s"))
 }
 
 console.log(correctErrors("M00kn155"))
+
+
+/*
+THREE THINGS I HAVE LEARNT:
+1. I have learnt about the replace method. It takes up 2 arguments which are: the pattern and the replacement. But it replaces the first occurence of the pattern
+2. I have learnt about the replaceAll method (which can be used when not using the regex). It takes up 2 arguments which are: the pattern and the replacement. But it replaces the all occurences of the pattern
+3. I have learnt about method chaining
+
+*/
