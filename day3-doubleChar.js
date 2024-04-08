3a4d25814e3a843aa17a8531a9ee5d052e2e5b8b
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-04-08 12:25:42 +0200

    Add Kata - Double Characters - 2

--- day3-doubleChar.js
+++ day3-doubleChar.js
@@ -6,12 +6,7 @@ Given a string, you have to return a string in which each character (case-sensit
 */
 
 function repeatChar(str) {
-    let doubleString = '';
-    for (let i = 0; i < str.length; i++) {
-        doubleString += str[i] + str[i];
-    }
-
-    return doubleString;
+    return str.split("").map((char) => char.repeat(2)).join("");
 
 }
 
