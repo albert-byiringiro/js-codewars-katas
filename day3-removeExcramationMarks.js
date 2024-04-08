5c770843d46db5a26389b4ece4f365b1ce8ca2bd
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-04-08 14:49:51 +0200

    Add Kata - Remove Excramation marks  - 2

--- day3-removeExcramationMarks.js
+++ day3-removeExcramationMarks.js
@@ -6,14 +6,7 @@ Write function RemoveExclamationMarks which removes all exclamation marks from a
 */
 
 function RemoveExclamationMarks(str) {
-    let newString = '';
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] !== '!') {
-            newString += str[i];
-        }
-    }
-
-    return newString;
+    return str.split("").filter((char) => char !== "!").join("");
 }
 
 console.log(RemoveExclamationMarks("Ru!k!ara!"))
