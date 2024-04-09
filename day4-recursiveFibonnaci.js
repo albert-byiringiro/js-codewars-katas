bd2f8cb445058263a6d83555398eed715e5bce5a
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-04-09 11:06:14 +0200

    Add Recursive Functions - Fibonnaci

---
+++ day4-recursiveFibonnaci.js
@@ -0,0 +1,11 @@
+// function to find and print nth fibonacci naumbers
+
+function fib(n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+
+console.log(fib(9))
