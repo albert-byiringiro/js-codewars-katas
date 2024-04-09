bb9db974dc4db3d07bddd906a121bea8f949c198
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-04-09 11:36:25 +0200

    Add comment to fibonnaci exercise

--- day4-factorial.js
+++ day4-factorial.js
@@ -1,11 +1,7 @@
 // The factorial of a natural number is a number multiplied by "number minus one", then by "number minus two", and so on till 1. The factorial of n is denoted as n!
 
 function factorial(n) {
-    if (n <= 1) {
-        return n;
-    } else {
-        return n * factorial(n - 1);
-    }
+    return (n <= 1) ? n : n * factorial(n - 1);
 }
 
 console.log(factorial(5)) //120
--- day4-recursiveFibonnaci.js
+++ day4-recursiveFibonnaci.js
@@ -1,5 +1,5 @@
 // function to find and print nth fibonacci naumbers
-
+// The sequence of Fibonacci numbers has the formula Fn = Fn-1 + Fn-2. In other words, the next number is a sum of the two preceding ones.
 function fib(n) {
     if (n <= 1) {
         return n;
