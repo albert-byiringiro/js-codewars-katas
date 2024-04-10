1e61517cd149061c43c1f8089b3e3e6909e45ae0
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-04-10 19:46:53 +0200

    Add Kata - Sum of the first nth term of Series - using reduce method

--- day5-seriesSum.js
+++ day5-seriesSum.js
@@ -7,14 +7,8 @@ Your task is to write a function which returns the sum of the following series u
 */
 
 function sumOfNth(n) {
-    let sum = 0;
-
-    for (let i = 0; i < n; i++) {
-        sum += 1 / (3 * i + 1);
-    }
-
-    return sum.toFixed(2);
+    return [...Array(n)].reduce((sum, curr, i) => sum + 1 / (3 * i + 1), 0).toFixed(2);
 }
 
-console.log(sumOfNth(2))
+console.log(sumOfNth(3))
 
