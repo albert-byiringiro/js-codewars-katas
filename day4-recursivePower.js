6677ca38933d488c3a97495589729c01e2eeb061
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-04-13 18:46:14 +0200

    Add New Structure to work with katas

--- day4-recursivePower.js
+++
@@ -1,5 +0,0 @@
-function pow(x, n) {
-    return (n === 1) ? x : x * pow(x, n - 1);
-}
-
-console.log(pow(2, 4));
--- day6-between.js
+++
@@ -1,7 +0,0 @@
-function between(a, b) {
-    let arr = []
-    for (let i = a; i <= b; i++) {
-        arr.push(i)
-    }
-    return arr;
-}
