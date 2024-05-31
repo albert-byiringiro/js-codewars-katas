433464d2b919d890d03a393da030dabc212b0f29
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-05-31 07:51:02 +0200

    kata: move zeros to the end of the array

---
+++ 5-kyu/moveZeros.js
@@ -0,0 +1,19 @@
+/*
+input: array
+output: new array with all zeros at the end of the array.
+steps:
+declare a function zeroMover with paramenter ar
+filter for elements which are not zeros
+filter for elements which are zeros
+return: concat the first filter to the second filter
+
+new thing learnt: filter returns an array of elements that meet the passed condition.
+*/
+
+let zeroMover = (arr) => {
+    return arr.filter(a => a !== 0).concat(arr.filter(a => a === 0))
+}
+
+let arr = [1, 8, 0, 9, 1, 0, 1];
+
+console.log(zeroMover(arr))
