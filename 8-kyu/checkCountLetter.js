bc046a1a2ee3f743a680ae1e86cddbc3b60a28d0
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-06-20 11:09:48 +0200

    refacter: All Star Code Challenge #18

--- 8-kyu/checkCountLetter.js
+++ 8-kyu/checkCountLetter.js
@@ -1,26 +1,4 @@
-const strCount = (str, letter) => {
-    /*
-    input: str, letter to check
-    output: count of occurences of the letter in the string
-    
-    steps:
-    declare let count=0
-    convert string to an array
-    iterate through the array
-    check if array contains the letter: if so increment the count other wise don't
-    
-    */
-
-    let count = 0;
-
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === letter) {
-            count++;
-        }
-    }
-
-    return count++;
-}
+const strCount = (str, letter) => str.split(letter).length - 1;
 
 console.log(strCount('Hello', 'l'));
 
