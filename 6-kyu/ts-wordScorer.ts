3a68b7b26b271fa20d5859c82b66d394a3fb142f
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-07-12 16:50:43 +0200

    add kata: find the word with highest count in a sentence

--- 6-kyu/ts-wordScorer.ts
+++ 6-kyu/ts-wordScorer.ts
@@ -1,10 +1,9 @@
 // https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/typescript
 
 export const high = (str: string): string => {
-    let st = str.split(" ").map(s => {
-      return [s, s.split("").reduce((o, c) => o+=c.charCodeAt(0)-96, 0)];
+    let score = str.split(" ").map(word => {
+      return word.split("").reduce((sum, chara) => sum += chara.charCodeAt(0)-96, 0);
     })
     
-    let result = st.sort((a, b) => Number(b[1])-Number(a[1]))[0][0];
-    return result as string;
+    return str.split(" ")[score.indexOf(Math.max(...score))];
   }
