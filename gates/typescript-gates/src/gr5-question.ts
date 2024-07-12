2faa6ab7a83998fe2881d5586024c5e9f95b079b
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-07-12 16:59:16 +0200

    add actual issue for group 5 question in the gate

--- gates/typescript-gates/src/gr5-question.ts
+++ gates/typescript-gates/src/gr5-question.ts
@@ -1,3 +1,33 @@
+/*
+THE ACTUAL ISSUE: FIX THIS
+
+function move(direction: string, distance: string) {}
+
+// TESTS
+
+move('up', 10);
+move('left', 5);
+
+move(
+  // @ts-expect-error - "up-right" is not a valid direction
+  'up-right',
+  10
+);
+
+move(
+  // @ts-expect-error - "down-left" is not a valid direction
+  'down-left',
+  20
+);
+
+move(
+  'up',
+  // @ts-expect-error - "20" is not a valid distance
+  '20'
+);
+
+*/ 
+
 type Direction = 'up' | 'down' | 'left' | 'right';
 type Distance = number;
 
