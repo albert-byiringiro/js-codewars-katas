faf00088013688db164d03befb3b984f3f130a2f
Albert Byiringiro <88356155+albert-byiringiro@users.noreply.github.com>
2025-02-06 08:20:39 +0200

    feature: added 8kyu kata - What's real floor - solution 2

--- 8-kyu/real-floor.js
+++ 8-kyu/real-floor.js
@@ -1,22 +1,3 @@
-function getRealFloor(n) {
-    if (n === 1 || n === 0) {
-        return 0
-    }
-
-    if (n === 13 || n === 14) {
-        return;
-    }
-
-    if (n >= 2 && n < 13) {
-        return n - 1
-    }
-
-    if (n >= 15) {
-        return n - 2
-    }
-
-    return n
-
+const getRealFloor = (n) => {
+    return n > 13 ? n - 2 : n > 0 ? n - 1 : n
 }
-
-console.log(getRealFloor(12))
